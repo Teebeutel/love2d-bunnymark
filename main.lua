4c5aebcc47745ca65a64ebda6bf35920a00cb8ff
Teebeutel <nightscheme@gmail.com>
2016-06-09 23:35:03 +0200

    Initial Commit with lots of small optimizations

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@
 function love.load()
     bunnies = {}
     gravity = 0.98
-    
+
     maxX = love.graphics.getWidth( )
     minX = 0
     maxY = love.graphics.getHeight( )
@@ -17,7 +17,7 @@ function love.load()
     baseLitterSize = 1000
     litterSizeIncrement = 1000
     litterSize = baseLitterSize
-    
+
     stdOutText = ""
 
     bunnyCount = 0
@@ -28,67 +28,57 @@ end
 function love.draw()
 
     love.graphics.print(bunnyCount .. " Total Bunnies", 20, 10)
-    
-    love.graphics.print(litterSize .. " bunnies in each Litter", 20, 20)
-    
-    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 20, 30)    
-    
-    for index,value in ipairs(bunnies) do
-        local tempBunnyId = value[1]
-        local tempBunnyPosX = value[2]
-        local tempBunnyPosY = value[3]
 
-        love.graphics.draw(bunnyImg, tempBunnyPosX, tempBunnyPosY)
+    love.graphics.print(litterSize .. " bunnies in each Litter", 20, 20)
+
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 20, 30)
+
+    for index,value in ipairs(bunnies) do
+        love.graphics.draw(bunnyImg, value.x, value.y)
     end
 
 end
 
-function love.mousepressed(x, y, button)
-    if button == 'l' then
-        for variable = 1, litterSize, 1 do
-            procreate(x, y)
-        end
-    elseif button == 'wu' then
-        litterSize = litterSize + litterSizeIncrement
-    elseif button == 'wd' then
-        if litterSize > baseLitterSize then 
-            litterSize = litterSize - litterSizeIncrement 
-        end
+function love.mousepressed(x, y, button, istouch)
+  if button == 1 then
+    for variable = 1, litterSize do
+      procreate(x, y)
     end
+  end
+end
+
+function love.wheelmoved(x, y)
+  if y > 0 then --the mousewheel was moved up
+      litterSize = litterSize + litterSizeIncrement
+  elseif y<0 then --the mousewheel was moved down
+      if litterSize > baseLitterSize then
+          litterSize = litterSize - litterSizeIncrement
+      end
+  end
 end
 
 function love.update(dt)
-    
-    for index,value in ipairs(bunnies) do
-        local tempBunnyId = value[1]
-        local tempBunnyPosX = value[2]
-        local tempBunnyPosY = value[3]
-        local tempBunnySpeedX = value[4]
-        local tempBunnySpeedY = value[5]
-        tempBunnyPosX = tempBunnyPosX + tempBunnySpeedX;    
-        tempBunnyPosY = tempBunnyPosY + tempBunnySpeedY;
 
-        tempBunnySpeedY = tempBunnySpeedY + gravity;
-        
-        if (tempBunnyPosX > maxX) then
-            tempBunnySpeedX = tempBunnySpeedX * -0.9;
-            tempBunnyPosX = maxX;
-        elseif (tempBunnyPosX < minX) then
-            tempBunnySpeedX = tempBunnySpeedX * -0.9;
-            tempBunnyPosX = minX;
+    for i=1,#bunnies do
+        bunnies[i].x = bunnies[i].x + bunnies[i].sx;
+        bunnies[i].y = bunnies[i].y + bunnies[i].sy;
+        bunnies[i].sy = bunnies[i].sy + gravity;
+
+        if (bunnies[i].x > maxX) then
+            bunnies[i].sx = bunnies[i].sx * -0.9;
+            bunnies[i].x = maxX;
+        elseif (bunnies[i].x < minX) then
+            bunnies[i].sx = bunnies[i].sx * -0.9;
+            bunnies[i].x = minX;
         end
-        
-        if (tempBunnyPosY > maxY) then
-            tempBunnySpeedY = tempBunnySpeedY * -0.9;
-            tempBunnyPosY = maxY;
-        elseif (tempBunnyPosY < minY) then
-            tempBunnySpeedY = tempBunnySpeedY * -0.9;
-            tempBunnyPosY = minY;
+
+        if (bunnies[i].y > maxY) then
+            bunnies[i].sy = bunnies[i].sy * -0.9;
+            bunnies[i].y = maxY;
+        elseif (bunnies[i].y < minY) then
+            bunnies[i].sy = bunnies[i].sy * -0.9;
+            bunnies[i].y = minY;
         end
-        
-        -- push all values back in the tables
-        local bunny = {tempBunnyId, tempBunnyPosX, tempBunnyPosY, tempBunnySpeedX, tempBunnySpeedY }
-        bunnies[index] = bunny
     end
 end
 function love.quit()
@@ -98,18 +88,9 @@ end
 -- Custom functions
 ------------------------------------------------
 
-function procreate(x,y) -- this function creates a new bunny
+function procreate(argx,argy) -- this function creates a new bunny
     bunnyCount = bunnyCount + 1
-    local bunnyId = bunnyCount
-    local bunnyPosX = x
-    local bunnyPosY = y
-    local bunnySpeedX = (math.random() * 10) - 5  
-    local bunnySpeedY = (math.random() * 10) - 5  
-    local bunny = {bunnyId, bunnyPosX, bunnyPosY, bunnySpeedX, bunnySpeedY }
-    
-    
-    table.insert(bunnies, bunny)
-
+    table.insert(bunnies, {id = bunnyCount, x=argx, y=argy, sx=(math.random() * 10) - 5, sy=(math.random() * 10) - 5 })
 end
 
 ------------------------------------------------
@@ -136,7 +117,7 @@ function table.val_to_str ( v )
     return "table" == type( v ) and table.tostring( v ) or
       tostring( v )
   end
-end 
+end
 
 function table.key_to_str ( k )
   if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
